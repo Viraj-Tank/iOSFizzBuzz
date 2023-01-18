b17419a0e985be46eb18e2c29898be67d7f3a3a5
VirajTank <virajtank98@gmail.com>
2023-01-18 11:05:24 +0530

    code create screen complete

--- iOSFizzBuzz/Model/FizzBuzzAlgorithmModel.swift
+++ iOSFizzBuzz/Model/FizzBuzzAlgorithmModel.swift
@@ -20,7 +20,7 @@ struct FizzBuzzAlgorithmModel: Identifiable {
     let programmingLanguageName: String
     var versionNumber: Double?
     var commentCode: (String) -> String = { code in
-        return "\\ " + code         // return "/*" + code "*/"
+        return #"\\ "# + code         // return "/*" + code "*/"
     }
     var imports: String = ""
     var boilerWrapper: (String) -> String = { code in
@@ -37,6 +37,42 @@ struct FizzBuzzAlgorithmModel: Identifiable {
     let elseStatement: (String) -> String
     
     var actualFizzBuzzCode: String {
-        return "FizzBuzz in \(programmingLanguageName)"
+        
+        let mod3 = "i \(moduleSymbol) 3 \(equality) 0"
+        let codeFizz = printString("Fizz")
+        
+        let mod5 = "i \(moduleSymbol) 5 \(equality) 0"
+        let codeBuzz = printString("Buzz")
+        
+        let mod15 = "i \(moduleSymbol) 15 \(equality) 0"
+        let codeFizzBuzz = printString("FizzBuzz")
+        
+        let printInt = printInt()
+        
+        let codeInLoop =
+        """
+        \(ifStatement(mod15,codeFizzBuzz))
+        \(elseIfStatement(mod5,codeFizz.indent()))
+        \(elseIfStatement(mod3,codeBuzz.indent()))
+        \(elseStatement(printInt.indent()))
+        """
+        
+        let mainCode =
+        """
+        \(maxConstDef)
+        \(loopWrapper(codeInLoop))
+        """
+        
+        var versionString = ""
+        
+        if let versionNumber = versionNumber {
+            versionString = " implementation Version \(versionNumber)"
+        }
+        
+        return """
+        \(commentCode("FizzBuzz\(versionString) is implemented in the \(programmingLanguageName) programming Language."))
+        \(imports)
+        \(mainCode)
+        """
     }
 }
