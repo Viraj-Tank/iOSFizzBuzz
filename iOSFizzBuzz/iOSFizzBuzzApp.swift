f2cc197ea55aeadc232ab182586d54f9d87e54b0
VirajTank <virajtank98@gmail.com>
2023-01-18 07:07:02 +0530

    Initial Commit

---
+++ iOSFizzBuzz/iOSFizzBuzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSFizzBuzzApp.swift
+//  iOSFizzBuzz
+//
+//  Created by Viraj Tank on 18/01/23.
+//
+
+import SwiftUI
+
+@main
+struct iOSFizzBuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
